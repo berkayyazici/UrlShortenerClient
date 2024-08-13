22062abfd0e99600ed92de4d8c75e76e0dfdcf33
Berkay Yazıcı <berkayyazici27@gmail.com>
2024-08-14 00:02:26 +0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
+import React, { useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Link, Router } from 'react-router-dom'
+import { Button } from 'primereact/button'
+import { InputText } from 'primereact/inputtext';
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+import { FloatLabel } from 'primereact/floatlabel';
+import { Dropdown } from 'primereact/dropdown';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState('');
+  const [selectedCity, setSelectedCity] = useState(null);
+  const cities = [
+    { name: 'testUrl0.com' },
+    { name: 'testUrl1.com' },
+    { name: 'testUrl2.com' },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="card flex justify-content-center">
+
+      <label>Shorten a long URL</label>
+      <br />
+      <br />
+      <InputText id="username" placeholder="Enter long link here" value={value} onChange={(e) => setValue(e.target.value)} />
+      <br />
+      <br />
+      <br />
+
+      <div className="card flex justify-content-center">
+
+        <label>Customize your link</label>
+        <br />
+        <br />
+        <Dropdown value={selectedCity} onChange={(e) => setSelectedCity(e.value)} options={cities} optionLabel="name" className="w-full md:w-14rem" />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
-} 
+}
 
 export default App
