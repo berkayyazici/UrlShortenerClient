8dd36acbe5f7cf6539951196c5996aa7071a589c
Berkay Yazıcı <berkayyazici27@gmail.com>
2024-08-15 00:27:54 +0300

    config & update for backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,87 @@
-import React, { useState } from "react";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from "react";
 import './App.css'
-import { Link, Router } from 'react-router-dom'
+import './style.css'
 import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext';
 import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
-import { FloatLabel } from 'primereact/floatlabel';
 import { Dropdown } from 'primereact/dropdown';
 
 function App() {
-  const [longLink, setlongLink] = useState('');
-  const [alias, setalias] = useState('');
-  const [selectedCity, setSelectedCity] = useState(null);
-  const cities = [
+  const [longLink, setLongLink] = useState('');
+  const [shortLink, setShortLink] = useState('');
+  const [alias, setAlias] = useState('');
+  const [selectedLink, setSelectedLink] = useState(null);
+  const [data, setData] = useState(null);
+  const url = "http://localhost:5066/api/Url";
+
+  const request = {
+    longUrl: longLink,
+    headerLink: selectedLink
+  }
+
+  const links = [
     'testUrl0.com', 'testUrl1.com', 'testUrl2.com',
   ];
 
+
+
+  function sendGetAllUrlRequest() {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(item => {
+          console.log(item.longUrl);
+        });
+      })
+      .catch(error => console.error('Error:', error));
+  }
+
+  function sendCreateShortUrlRequest() {
+    fetch(url + '/GetShortUrl', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ request })
+      //body: 'longUrl=' + { longLink } + '&headerLink=' + { alias }
+    })
+      .then(response => { response.json() })
+      .then(data => { })
+      .catch(error => console.error('Error:', error));
+  }
+
+
   return (
-    <div className="card flex justify-content-center">
+
+    <div className="card flex flex-wrap justify-content-center ">
 
       <label>Shorten a long URL</label>
       <br />
       <br />
-      <InputText id="longLink" placeholder="Enter long link here" value={longLink} onChange={(e) => setlongLink(e.target.value)} />
+      <InputText id="longLink" placeholder="Enter long link here" value={longLink} onChange={(e) => setLongLink(e.target.value)} />
       <br />
       <br />
       <br />
 
-      <div className="card flex flex-wrap justify-content-center gap-6">
+      <div className="card flex flex-wrap justify-content-center gap-3">
 
         <label>Customize your link</label>
         <br />
         <br />
-        <Dropdown placeholder="Select a URL template" value={selectedCity} onChange={(e) => { setSelectedCity(e.value); }} options={cities} optionLabel="name" className="w-full md:w-14rem" />
-        <InputText id="alias" placeholder="Enter alias" value={alias} onChange={(e) => setalias(e.target.value)} />
+        <Dropdown placeholder="Select a URL template" style={{ marginRight: '10px' }} value={selectedLink} onChange={(e) => { setSelectedLink(e.value); }} options={links} optionLabel="name" className="w-full md:w-14rem" />
+        <InputText id="alias" placeholder="Enter alias" value={alias} onChange={(e) => setAlias(e.target.value)} />
       </div>
+
+      <div className="card flex flex-wrap justify-content-center gap-3">
+        <Button onClick={sendCreateShortUrlRequest} className="button-spacing">Create Shorten URL</Button>
+        <Button onClick={sendGetAllUrlRequest}>Get All Shorten URL</Button>
+      </div>
+
+
+      <div style={{ isVisible: 'false' }}>
+        <label > Your shorten link :  {shortLink}</label>
+      </div>
+
 
     </div>
   )
